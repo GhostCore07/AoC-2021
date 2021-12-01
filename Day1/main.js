e8c65c2215a28e0b5dfed111db534e1202c3acb2
GhostCore07 <ghostcore07@protonmail.com>
2021-12-01 04:28:30 -0500

    JS day 1 solution

---
+++ Day1/main.js
@@ -0,0 +1,18 @@
+import {input_data} from "./data.js"
+
+let lines = input_data.split("\n");
+lines = lines.map(el => Number(el));
+console.log(lines);
+
+let count = 0;
+for (let i = 0; i < lines.length - 1; i++)
+{
+	if (lines[i] < lines[i+1]) count++;
+}
+console.log(count);
+count = 0;
+for (let i = 0; i < lines.length - 3; i++)
+{
+	if (lines[i]+lines[i+1]+lines[i+2] < lines[i+1]+lines[i+2]+lines[i+3]) count++;
+}
+console.log(count);
