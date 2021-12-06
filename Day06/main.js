e949b4c4afccb05ec694b653c0e6d856155c75c1
GhostCore07 <ghostcore07@protonmail.com>
2021-12-06 01:28:29 -0500

    functionalize

--- Day06/main.js
+++ Day06/main.js
@@ -1,21 +1,27 @@
 import {input_data} from "./data.js"
 
-let lines = input_data.split(",").map(Number);
-let fishes = [0, 0, 0, 0, 0, 0, 0, 0, 0];
+function computeLanternFish(data, days)
+{
+	const lines = data.split(",").map(Number);
+	const fishes = [0, 0, 0, 0, 0, 0, 0, 0, 0];
 
-for (let i = 0; i < lines.length; i++)
-{
-	fishes[lines[i]]++;
+	for (let i = 0; i < lines.length; i++)
+	{
+		fishes[lines[i]]++;
+	}
+	for (let d = 0; d < days; d++)
+	{
+		const s = fishes.shift();
+		fishes[8] = s;
+		fishes[6] += s;
+	}
+	let count = 0;
+	for (let i = 0; i < 9; i++)
+	{
+		count += fishes[i];
+	}
+	console.log(count)
 }
-for (let d = 0; d < 256; d++)
-{
-	let s = fishes.shift();
-	fishes[8] = s;
-	fishes[6] += s;
-}
-let count = 0;
-for (let i = 0; i < 9; i++)
-{
-	count += fishes[i];
-}
-console.log(count)
+
+computeLanternFish(input_data, 80);
+computeLanternFish(input_data, 256);
