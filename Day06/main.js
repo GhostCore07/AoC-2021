cac2cc45ccf80c27df366cbe83ab07a65fa119b9
GhostCore07 <ghostcore07@protonmail.com>
2021-12-06 00:59:22 -0500

    JS day 6 solution

---
+++ Day06/main.js
@@ -0,0 +1,21 @@
+import {input_data} from "./data.js"
+
+let lines = input_data.split(",").map(Number);
+let fishes = [0, 0, 0, 0, 0, 0, 0, 0, 0];
+
+for (let i = 0; i < lines.length; i++)
+{
+	fishes[lines[i]]++;
+}
+for (let d = 0; d <= 256; d++)
+{
+	let s = fishes.shift();
+	fishes[8] = s;
+	fishes[6] += s;
+}
+let count = 0;
+for (let i = 0; i < 8; i++)
+{
+	count += fishes[i];
+}
+console.log(count)
